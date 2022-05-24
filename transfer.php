c6af9cec8fef7e5f727d452c67986d44cbc14ea9
Pablo Ardito <pabloardito@MacBook-Pro-de-Pablo.local>
2022-05-24 09:17:32 -0300

    transfer

---
+++ transfer.php
@@ -0,0 +1,93 @@
+
+<?php include('includes/header--white.php') ?>
+
+
+<main class="c-hotel">
+        
+    <section class="c-hotel__banner" style="background-image:url('assets/img/transfer.jpg');">
+
+    </section>
+ 
+    <section class="pt-5 c-hotel__content">
+
+        <div class="container">
+        <div class="row mb-5">
+                <div class="col-xl-12">
+                    <h2 class="d-inline-block fs-5 text-primary p-2 px-0 mb-0">Transfer</h2>
+                    <h1 class="mb-2 fs-3 fw-bold text-secondary">TRF O.W. PORTO / ARPT OU V.V</h1>
+                    <p class="text-black-50">SERVICIOS EN PRIVADO</p>
+
+                    </div>
+        </div>
+            <div class="row">
+                <div class="col-xl-7">
+                    
+                    <ul class="nav nav-tabs d-flex justify-content-between">
+                        <li class="nav-item">
+                            <div class="nav-link active" id="sobre-tab" data-bs-toggle="tab" data-bs-target="#sobre" type="button" role="tab" aria-controls="sobre" aria-selected="true">
+                                <span class="icon icon-map me-3"></span>Sobre o Destino
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="nav-link" id="apartamentos-tab" data-bs-toggle="tab" data-bs-target="#apartamentos" type="button" role="tab" aria-controls="apartamentos" aria-selected="false">
+                                <span class="icon icon-children me-3"></span>Política de menores
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="nav-link" id="localizacao-tab" data-bs-toggle="tab" data-bs-target="#localizacao" type="button" role="tab" aria-controls="localizacao" aria-selected="false">
+                                <span class="icon icon-info1 me-3"></span>Observações
+                            </div>
+                        </li>
+                       
+                    </ul>
+
+
+                    <div class="tab-content mt-5" id="myTabContent">
+                        <div class="tab-pane fade show active" id="sobre" role="tabpanel" aria-labelledby="home-tab">
+                            
+                            <div class="pb-5 fs-6">
+
+
+                                <p>A movimentada cidade do Rio de Janeiro tem sido um dos destinos turísticos Brasileiros mais populares e frequentados há décadas. Sua localização é um privilégio, cercado por montanhas que faz um contorno suave pelo horizonte. Florestas tropicais e vistas deslumbrantes sobre o Oceano Atlântico fazem com que esta cidade mereça o título de "Cidade Maravilhosa". Os Cariocas (que nascem no Rio de Janeiro), são sempre alegres e acolhedores.
+                                </p><p>Os pontos turísticos mais populares da cidade são a Estátua do Cristo Redentor e a Montanha do Pão de Açúcar. Outros pontos importantes são; Floresta da Tijuca, a maior floresta urbana do mundo, Arcos da Lapa, o centro histórico da cidade e muitas praias bonitas como Copacabana, Ipanema, Leblon e Barra da Tijuca, que é a maior praia de a cidade. O bairro da Barra da Tijuca é o lar de modernos e mega-shoppings, como o BarraShopping, o New York City Center, o Rio Design Barra e o Downtown. Ainda existe o Village Mall, o shopping mais exclusivo e sofisticado do Rio de Janeiro. O bairro tem alguns dos melhores restaurantes da cidade, cujas culinárias representam os países da Itália, Japão, França, Portugal, Alemanha, Oriente Médio entre outros. A Barra da Tijuca foi um dos bairros onde se realizaram os Jogos Olímpicos e Paralímpicos Rio 2016.
+                                </p><p>
+Boa parte da história do Brasil – em especial do período Imperial - está guardada no Rio de Janeiro. Um dos mais glamourosos prédios da Cinelândia, o Theatro Municipal foi inspirado na Ópera de Paris e inaugurado em 1909. Em seu interior estão arcadas, balaustradas, colunas e escadarias de mármore, esculturas em bronze e vitrais importados da Europa. Museu de Arte Moderno do Rio de Janeiro, o prédio modernista representa um marco na arquitetura brasileira, resultado das linhas retas do arquiteto Affonso Eduardo Reidy e do projeto paisagístico de Roberto Burle Marx. Inaugurado em 1958 e reativado em 1990, o museu sedia grandes exposições nacionais e internacionais. Inaugurado no fim de 2015 e Museu do Amanhã coroou a Praça Mauá, que desde 2013, com a abertura do Museu de Arte do Rio (MAR), vem recuperando com sucesso seu glamour. O Museu do Amanhã é um museu de ciências dedicado a explorar as possibilidades de construção do futuro.</p>
+</div>
+
+                            
+                            
+                        </div>
+                        <div class="tab-pane fade" id="apartamentos" role="tabpanel" aria-labelledby="profile-tab">
+                        <p>Infantes de 0 a 4 anos: A cada 2 adultos aceita 1 infante, não ocupa assento.</p>
+                        </div>
+                        <div class="tab-pane fade" id="localizacao" role="tabpanel" aria-labelledby="contact-tab">
+                        
+                        </div>
+                        <div class="tab-pane fade" id="destinos" role="tabpanel" aria-labelledby="contact-tab">
+                             d   
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-xl-4 offset-xl-1">
+                    <h2 class="fs-28 text-secondary mb-2 fw-bold">Mínimo de Pax</h2>
+                    <p>-</p>
+                    <hr class="mt-4 mb-4">
+                    <h2 class="fs-28 text-secondary mb-2 fw-bold">Idiomas</h2>
+                    <p>Português<br>Espanhol<br>Ver en Condiciones</p>
+                    <hr class="mt-4 mb-4">
+                    <h2 class="fs-28 text-secondary mb-2 fw-bold">Frequência</h2>
+                    <p>Diária</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</main>
+
+<?php include('includes/footer.php') ?>
+
+<script src="assets/js/pages/hotel.js"></script>
+
